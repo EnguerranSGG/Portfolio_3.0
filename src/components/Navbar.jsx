c145c130ed794fc86b368fe0d0e2d92689156a28
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2024-02-20 11:54:00 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,57 @@
+import React, {useEffect, useState} from 'react';
+import { Link } from 'react-router-dom';
+
+import { styles } from '../styles';
+import { navLinks } from '../constants';
+import { logo, menu, close } from '../assets';
+
+const Navbar = () => {
+
+  const [active, setActive] = useState('');
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <nav className="navbar" /**{`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}**/>
+      <div className='navbar_logo_text'>
+        <Link to='/' className='navbar_logo_link' onClick={() => {
+          setActive("")
+          window.scrollTo(0, 0)
+          }}>
+        <img src={logo} alt="logo" className='navbar_logo'/>
+        <p className='logo_text'>Enguerran Smagghe<br/><span className='no_title'>| Concepteur développeur d'applications</span></p>
+        </Link>
+        <ul className='navbar_links'>
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+        <div className='navbar_menu'>
+          <img src={toggle ? close : menu} alt="menu" className='menu_img'
+            onClick={() => setToggle(!toggle)} />
+          <div className={`${!toggle ? 'no_menu' : 'show_menu'}`}>
+            <ul className='menu_links'>
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(link.title);
+                  }}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>  
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
