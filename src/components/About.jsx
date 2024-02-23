d0eba834673e5cdd8ac9f15396f36d7b21d4ce4d
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2024-02-23 13:04:58 +0100

    Complete about section / correct responsive mobile

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,8 +18,8 @@ const About = () => {
       </motion.div>
 
       <motion.p variants={fadeIn("", "", 0.1, 1)} className='intro_description'>
-        Enguerran, 25 ans, ancien science politique en reconversion professionnelle depuis un an maintenant.
-        Un profil à la croisée de deux chemins. Parler de mon intérêt pour C++ et C#.
+        Ancien "science politiste" de 25 ans, cela va bientôt faire un an maintenant que je me suis reconverti en tant
+        développeur. Ce qui m'a tout de suite plu dans ce secteur ? Son champ des possibles et sa transversalité !<br /><br />
       </motion.p>
 
       <motion.div variants={fadeIn("", "", 0.1, 1)} className='academic'>
@@ -33,11 +33,14 @@ const About = () => {
         Issu à l'origine d'un parcours universitaire,
         j'ai effectivement arpenté les chemins de
         la science politique jusqu'à la validation
-        de mon M1. <br />
-
-        Au delà des connaissances pures et de la
+        de mon M1. Au delà des connaissances pures et de la
         maîtrise de l'anglais, j'y ai acquis un grand
-        sens de l'organisation et de la rigueur !
+        sens de l'organisation et de la rigueur ! <br /><br />
+        En plus des savoirs et compétences académiques, au fil des années, j'ai eu l'occasion de participer à de
+        nombreux travaux de groupe et projets. Ils m'ont permis de gagner une expérience notoire dans la gestion de projet.
+        Expérience que j'aspire consolider et concrétiser à l'avenir.
+
+
       </motion.p>
 
       <motion.div variants={fadeIn("", "", 0.1, 1)} className='active'>
@@ -48,14 +51,13 @@ const About = () => {
       </motion.div>
 
       <motion.p variants={fadeIn("", "", 0.1, 1)} className='active_description'>
-        Issu à l'origine d'un parcours universitaire,
-        j'ai effectivement arpenté les chemins de
-        la science politique jusqu'à la validation
-        de mon M1. <br />
-
-        Au delà des connaissances pures et de la
-        maîtrise de l'anglais, j'y ai acquis un grand
-        sens de l'organisation et de la rigueur !
+        Après ma période d'apprentissage en autodidacte, lorsque j'eus envie de passer aux choses sérieuses, je rejoignis
+        le centre de formation Simplon. Le fait d'avoir toujours travaillé sur des projets
+        concrêts avec leur deadline et une grande autonomie m'a fait gagner en confiance et en débrouillardise, compétences
+        essentielles pour un développeur. <br /><br />
+        Maintenant, je n'aspire plus qu'à une seule chose, poursuivre mon apprentissage. Acquérir un socle de connaissances
+        encore plus solides avant de me spécialiser. Je porte notamment un grand intêret pour les technologies de réalité
+        augmentée et de réalité virtuelle, c'est pourquoi j'ai commencé à apprendre C# et C++.
       </motion.p>
 
     </>
