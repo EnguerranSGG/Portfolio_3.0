fd8a069fa2a13361c07a61a0e4a8140b021d442d
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2024-02-27 16:17:31 +0100

    Complete tablet responsivity

--- src/components/canvas/VRHeadset.jsx
+++ src/components/canvas/VRHeadset.jsx
@@ -4,7 +4,7 @@ import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 import CanvasLoader from '../Loader'
 
-const VRHeadset = ({ isMobile, isUnderHeight400 }) => {
+const VRHeadset = ({ isMobile, isUnderHeight400, isTablet }) => {
   const vrHeadset = useGLTF('./apple_vision_pro/scene.gltf')
 
   return (
@@ -14,7 +14,7 @@ const VRHeadset = ({ isMobile, isUnderHeight400 }) => {
       <spotLight position={[50, 10, 50]} angle={0.12} penumbra={2} intensity={15000} castShadow shadow-mapSize={1024} distance={100000}/>
       <primitive 
       object={vrHeadset.scene}
-      scale={ isUnderHeight400 ? 50 : isMobile ? 32 : 60}
+      scale={ isUnderHeight400 ? 50 : isTablet ? 50 : isMobile ? 32 : 60}
       position={ isMobile ? [0, 0, 0] : [0, 0.5, -0.5]}
       rotation={[-0.1, -0.9, -0.2]}
       />
@@ -26,6 +26,7 @@ const VRHeadsetCanvas = () => {
 
   const [isMobile, setIsMobile] = useState(false)
   const [isUnderHeight400, setIsUnderHeight400] = useState(false)
+  const [isTablet, setIsTablet] = useState(false)
 
   useEffect(() => {
 
@@ -33,11 +34,13 @@ const VRHeadsetCanvas = () => {
 
     const mediaQuery = window.matchMedia('(max-width: 800px)')
     const underHeight400 = window.matchMedia('(max-height: 400px)')
+    const tablet = window.matchMedia('(min-width: 769px) and (max-width: 1290px)')
 
     // Set the initial value of the `isMobile` state variable
 
     setIsMobile(mediaQuery.matches)
     setIsUnderHeight400(underHeight400.matches)
+    setIsTablet(tablet.matches)
 
     // Define a callback function to handle changes to the media query
 
@@ -45,6 +48,7 @@ const VRHeadsetCanvas = () => {
 
       setIsMobile(event.matches)
       setIsUnderHeight400(event.matches)
+      setIsTablet(event.matches)
 
     }
 
@@ -52,6 +56,7 @@ const VRHeadsetCanvas = () => {
 
     mediaQuery.addEventListener('change', handleMediaQueryChange)
     underHeight400.addEventListener('change', handleMediaQueryChange)
+    tablet.addEventListener('change', handleMediaQueryChange)
 
     // Remove the listener when the component is unmounted
 
@@ -59,6 +64,7 @@ const VRHeadsetCanvas = () => {
     
       mediaQuery.removeEventListener('change', handleMediaQueryChange)
       underHeight400.removeEventListener('change', handleMediaQueryChange)
+      tablet.removeEventListener('change', handleMediaQueryChange)
 
     }
   })
@@ -73,7 +79,7 @@ const VRHeadsetCanvas = () => {
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
-        <VRHeadset isMobile={isMobile} isUnderHeight400={isUnderHeight400} />
+        <VRHeadset isMobile={isMobile} isUnderHeight400={isUnderHeight400} isTablet={isTablet} />
       </Suspense>
       <Preload all />
     </Canvas>
