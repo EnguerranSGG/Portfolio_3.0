fff211a7b071a6cedb05084f618915b500197ea9
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2024-02-27 14:55:44 +0100

    Finalyse mobile responsivty (landscape)

--- src/components/canvas/VRHeadset.jsx
+++ src/components/canvas/VRHeadset.jsx
@@ -4,7 +4,7 @@ import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 import CanvasLoader from '../Loader'
 
-const VRHeadset = ({ isMobile }) => {
+const VRHeadset = ({ isMobile, isUnderHeight400 }) => {
   const vrHeadset = useGLTF('./apple_vision_pro/scene.gltf')
 
   return (
@@ -14,7 +14,7 @@ const VRHeadset = ({ isMobile }) => {
       <spotLight position={[50, 10, 50]} angle={0.12} penumbra={2} intensity={15000} castShadow shadow-mapSize={1024} distance={100000}/>
       <primitive 
       object={vrHeadset.scene}
-      scale={ isMobile ? 32 : 60}
+      scale={ isUnderHeight400 ? 50 : isMobile ? 32 : 60}
       position={ isMobile ? [0, 0, 0] : [0, 0.5, -0.5]}
       rotation={[-0.1, -0.9, -0.2]}
       />
@@ -25,34 +25,40 @@ const VRHeadset = ({ isMobile }) => {
 const VRHeadsetCanvas = () => {
 
   const [isMobile, setIsMobile] = useState(false)
+  const [isUnderHeight400, setIsUnderHeight400] = useState(false)
 
   useEffect(() => {
 
     // Add a listener for changes to the screen size
 
     const mediaQuery = window.matchMedia('(max-width: 800px)')
+    const underHeight400 = window.matchMedia('(max-height: 400px)')
 
     // Set the initial value of the `isMobile` state variable
 
     setIsMobile(mediaQuery.matches)
+    setIsUnderHeight400(underHeight400.matches)
 
     // Define a callback function to handle changes to the media query
 
     const handleMediaQueryChange = (event) => {
 
       setIsMobile(event.matches)
+      setIsUnderHeight400(event.matches)
 
     }
 
     // Add the callback function as a listener for changes to the media query
 
     mediaQuery.addEventListener('change', handleMediaQueryChange)
+    underHeight400.addEventListener('change', handleMediaQueryChange)
 
     // Remove the listener when the component is unmounted
 
     return () => {
     
       mediaQuery.removeEventListener('change', handleMediaQueryChange)
+      underHeight400.removeEventListener('change', handleMediaQueryChange)
 
     }
   })
@@ -67,7 +73,7 @@ const VRHeadsetCanvas = () => {
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
-        <VRHeadset isMobile={isMobile} />
+        <VRHeadset isMobile={isMobile} isUnderHeight400={isUnderHeight400} />
       </Suspense>
       <Preload all />
     </Canvas>
