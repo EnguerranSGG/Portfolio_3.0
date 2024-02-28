a5084274b073ab8b25b90edf1b3cd2d59d46bda4
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2024-02-28 23:40:41 +0100

    Create a footer with contact's link

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer>
+            <a href="https://github.com/EnguerranSGG" target="_blank"><i class="fa-brands fa-github"></i></a>
+            <a href="mailto:enguerran.smagghe@hotmail.fr"><i class="fa-solid fa-envelope"></i></a>
+            <a href="https://www.linkedin.com/in/enguerran-smagghe/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+        </footer>
+    );
+};
+
+export default Footer
