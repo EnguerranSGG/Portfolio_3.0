80f0af8eb6e0c376bb1228b632171f3d49fdea7d
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2024-05-09 02:27:45 +0200

    Momentary put in comment some img format changes

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -21,7 +21,7 @@ const Ball = (props) => {
   )
 }
 
-const BallCanvas = ({ icon, iconAVIF }) => {
+const BallCanvas = ({ icon /*, iconAVIF */ }) => {
 
   return (
     <Canvas
@@ -31,7 +31,7 @@ const BallCanvas = ({ icon, iconAVIF }) => {
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false}/>
-        <Ball imgUrl={icon} onError={e => e.currentTarget.src = `${iconAVIF}`} />
+        <Ball imgUrl={icon} /* onError={e => e.currentTarget.src = `${iconAVIF}`}*/ />
       </Suspense>
       <Preload all />
     </Canvas>
