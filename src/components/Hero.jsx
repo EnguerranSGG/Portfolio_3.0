1e1080d96cf55247ea6511a13365e016bcfcd142
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2024-10-17 20:41:44 +0200

    refactor(Heros.jsx): change description

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,31 +1,37 @@
-import React from 'react'
+import React from "react";
 
-import { profilePic } from '../assets'
+import { profilePic } from "../assets";
 
-import { SectionWrapper } from '../hoc'
+import { SectionWrapper } from "../hoc";
 
 const Hero = () => {
   return (
     <>
-    <section className="about">
-      <div className='about_div'>
-        <div className='about_features'>
-          <div className='about_ball'/>
-          <div className='about_line'/>
+      <section className="about">
+        <div className="about_div">
+          <div className="about_features">
+            <div className="about_ball" />
+            <div className="about_line" />
+          </div>
+          <div className="about_text">
+            <div>
+              <h1>
+                Bonjour, je suis <span>Enguerran</span>
+              </h1>
+            </div>
+            <p>
+              Un jeune développeur en quête d'un stage pour valider son titre
+              CDA !
+            </p>
+          </div>
         </div>
-        <div className='about_text'>
-          <div><h1>Bonjour, je suis <span>Enguerran</span></h1></div>
-          <p>Un jeune développeur en quête d'une alternance pour sa formation de développeur web !</p>
+
+        <div className="about_image">
+          <img src={profilePic} alt="profile" />
         </div>
-      </div>
-
-      <div className='about_image'>
-        <img src={profilePic} alt="profile" />
-      </div>
-
-    </section>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default SectionWrapper(Hero , "")
+export default SectionWrapper(Hero, "");
