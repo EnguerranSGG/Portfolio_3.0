228f22bedb483df6c85173ff24824b632728187c
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2025-03-16 15:31:31 +0100

    feat(components): review about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,33 +17,29 @@ const About = () => {
       </motion.div>
 
       <motion.p variants={fadeIn("", "", 0.1, 1)} className='intro_description'>
-        Issu d’une formation en sciences politiques spécialisée dans la conduite de projets européens, j’ai décidé il y a un an et demi de me réorienter vers l’informatique, un domaine qui allie rigueur, créativité et innovation. Mon intérêt pour la programmation m’a rapidement conduit à explorer la data science et l’intelligence artificielle, des domaines où l’analyse et la modélisation de données jouent un rôle central. <br /><br />
+      Issu d’une formation en science politique spécialisée dans la conduite de projets européens, j’ai décidé il y a deux ans de me reconvertir dans le développement informatique. Ce choix est né du désir de travailler dans un domaine où logique, créativité et innovation se rencontrent. Après une première initiation autodidacte au HTML et CSS, j’ai rapidement élargi mes compétences en intégrant une formation intensive chez Simplon, où j’ai découvert la programmation back-end et front-end à travers divers langages, librairies et frameworks. <br/><br/>
       </motion.p>
 
       <motion.div variants={fadeIn("", "", 0.1, 1)} className='academic'>
         <div className='academic_line' />
         <h3 className='academic_title'>
-          De la rigueur académique à l’analyse de données
+        Un apprentissage structuré et professionnalisant
         </h3>
       </motion.div>
 
       <motion.p variants={fadeIn("", "", 0.1, 1)} className='academic_description'>
-        Mon parcours universitaire m’a apporté une solide capacité d’analyse, une organisation rigoureuse et une bonne maîtrise de l’anglais. En travaillant sur des projets de recherche et des études comparatives, j’ai développé un esprit critique et méthodique, des compétences essentielles en data science et en machine learning. <br /><br />
-        Au fil des années, j’ai également acquis une expérience significative en gestion de projet et en travail d’équipe. Cette capacité à structurer, organiser et collaborer efficacement me permet aujourd’hui d’aborder des problématiques complexes liées à l’analyse de données et à l’apprentissage automatique.
+      Au fil de mon parcours, j’ai consolidé mes connaissances en conception de solutions numériques et en gestion de bases de données. Mon expérience en développement s’est enrichie au travers de plusieurs projets concrets : création d’un site vitrine en React lors de mon premier stage, développement d’une application de gestion en .NET, et participation à un projet collaboratif visant à automatiser la gestion d’un serveur Discord. J’ai également été sensibilisé à la philosophie DevOps, à la conteneurisation avec Docker et aux pipelines CI/CD.
       </motion.p>
 
       <motion.div variants={fadeIn("", "", 0.1, 1)} className='active'>
         <h3 className='active_title'>
-          Un apprentissage progressif et pragmatique
+          Vers l'intelligence artificielle...
         </h3>
         <div className='active_line' />
       </motion.div>
 
       <motion.p variants={fadeIn("", "", 0.1, 1)} className='active_description'>
-        Après une période d’apprentissage en autodidacte, j’ai intégré Simplon pour une formation intensive en développement logiciel. J’y ai acquis des compétences solides en programmation (Java, .NET, TypeScript) et en gestion des bases de données (PostgreSQL, SQL). Ce parcours m’a permis de travailler sur des projets concrets avec des contraintes réelles, renforçant ainsi ma capacité à résoudre des problèmes complexes de manière efficace et autonome. <br /><br />
-        Aujourd’hui, je me spécialise dans l’intelligence artificielle et la data science. J’explore activement les fondamentaux du machine learning, de l’analyse de données et des systèmes de recommandation. Mon objectif est d’acquérir une expertise technique approfondie pour contribuer à des projets innovants dans ces domaines. <br /><br />
-        Je suis actuellement à la recherche d’une <strong>alternance pour la rentrée 2025</strong>, afin de consolider mes compétences et d’évoluer au sein d’une équipe spécialisée en IA et en data science.
-      </motion.p>
+      Aujourd’hui, je me consacre pleinement à l’apprentissage de la data science, de l’algèbre linéaire et des fondamentaux de l’intelligence artificielle, avec pour objectif de devenir Machine Learning Engineer. Depuis longtemps, l’IA me fascine, non seulement par son potentiel à transformer nos sociétés, mais aussi par les défis techniques et éthiques qu’elle soulève. Convaincu que nous sommes à l’aube d’une révolution où la donnée et l’apprentissage automatique joueront un rôle central, je souhaite approfondir mon expertise et contribuer activement à cette dynamique.<br/><br/>Dans cette optique, je suis à la recherche d’une alternance pour la rentrée 2025, afin de perfectionner mes compétences, évoluer dans un cadre professionnel stimulant et participer à des projets d’innovation en intelligence artificielle.      </motion.p>
     </>
   )
 }
